6a7e43f96c1c3f4199e33e41bee039930e59b102
Pouria <pouria.khakpour9909@gmail.com>
2022-10-21 05:30:53 +0330

    Seperate start query handler from utils functions

--- internal/bot/utils.go
+++ internal/bot/utils.go
@@ -172,33 +172,6 @@ func IsStartQuery(text string) bool {
 		return false
 	}
 }
-
-// Start query handler, gets the book id and returns its information
-func StartQueryHandler(bot_api *tgbotapi.BotAPI, update *tgbotapi.Update) {
-	// Extract book id from query
-	book_id, err := extractBookIDFromStartQuery(update.Message.Text)
-	if err != nil {
-		log.Printf("Error occurred during extract book id from start query - %s", err.Error())
-		SendUnknownError(bot_api, update.FromChat().ChatConfig().ChatID)
-		return
-	}
-	// Format book information as text to send
-	book_formatted_info, err := formatBookInformation(book_id)
-	if err != nil {
-		log.Printf("Error occurred during format book information - %s", err.Error())
-		SendUnknownError(bot_api, update.FromChat().ChatConfig().ChatID)
-	}
-	// Send result
-	msg := tgbotapi.NewMessage(update.FromChat().ChatConfig().ChatID, book_formatted_info)
-	msg.ReplyMarkup = nil
-	if update.Message != nil && update.Message.MessageID != 0 {
-		msg.ReplyToMessageID = update.Message.MessageID
-	}
-	if _, err := bot_api.Send(msg); err != nil {
-		log.Printf("Error occurred during send book information message - %s", err.Error())
-		SendUnknownError(bot_api, update.FromChat().ChatConfig().ChatID)
-	}
-}
 func extractBookIDFromStartQuery(query string) (int, error) {
 	id_str := strings.Split(query, "=")[1]
 	return strconv.Atoi(id_str)
